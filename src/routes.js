b45ea33bc1cd952873a3a68cfef4b5e4cd7308ad
oliveiraemerson <emerson.oliveira@edesoft.com.br>
2020-04-19 19:15:26 -0300

    Módulo finalizado

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ const routes = express.Router();
 const ProdutController = require('./controllers/ProductController');
 
 routes.get('/products', ProdutController.index);
+routes.get('/products/:id', ProdutController.show);
 routes.post('/products', ProdutController.store);
+routes.put('/products/:id', ProdutController.update);
+routes.delete('/products/:id', ProdutController.destroy);
 
 module.exports = routes;
